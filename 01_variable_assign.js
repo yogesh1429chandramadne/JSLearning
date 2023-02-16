d5477f1bcdd12dff6a5dcdd732dd93f3aae998c0
yogesh1429chandramadne <yogesh1429.yc@gmail.com>
2023-02-16 20:44:09 +0530

    01_variable assign
    01_variable assign

---
+++ 01_variable_assign.js
@@ -0,0 +1,15 @@
+console.log("********step 1********");
+
+console.log("myFullName : yogesh chandramadne");
+ console.log("myFullName with middle name : yogesh Dhondiba Chandramadne ");
+console.log("********* step 2 ***********");
+console.log(" I Love Only JS");
+console.log("********** step *************");
+console.log("First Name : yogesh")
+console.log("Last Name : chandramadne");
+console.log("Age : 28");
+console.log("Address : Nanded");
+console.log("pinCode : 431741");
+console.log("Gender : male");
+
+console.log("IsMarried : no");
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,7 @@
 console.log("mohit sharma");
+console.log( " yogesh chandramadne");
+console.log(" 27");
+console.log("pune");
+var city = "pune";
+console.log("city");
+
