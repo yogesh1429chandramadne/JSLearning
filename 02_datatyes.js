d88fd53ed0554460aa9743c4b9b6eb02485f659f
yogesh1429chandramadne <yogesh1429.yc@gmail.com>
2023-02-17 13:07:55 +0530

    02_assignments
    02_assignments

---
+++ 02_datatyes.js
@@ -0,0 +1,18 @@
+var myName = 'Virat Kohli';
+var fatherName = "Yogesh Kohli"
+
+var age = 31;
+var weight = 82.5;
+
+var isMarried = true;
+var isLazy = false; 
+
+var typeOfMyName  = typeof myName;
+console.log("Typeof variable myName: ",  typeOfMyName);
+console.log("typeOf variable fatherName:", typeof fatherName);
+console.log("typeof variable weight:",typeof weight);
+console.log("typeof variable isMarried:",typeof isMarried);
+var graduation;
+console.log("Value of  graduation variable is: ", graduation);
+console.log("typeOf graduation is: ", graduation);
+console.log("typeof age:",typeof age);
---
+++ 02_typeOf.js
@@ -0,0 +1,12 @@
+var bankName = "SBI Bank"; 
+console.log("bankName:",bankName);
+console.log("typeof bankName:",typeof bankName) 
+var marks =  "90%"
+console.log("marks:",marks);
+console.log("typeof marks:",typeof marks);
+var isWorking = true;
+console.log(" isWorking :", isWorking );
+console.log("typeof  variable isWorking:",typeof isWorking);
+var totalCount = 107;
+console.log("totalCount:",totalCount);
+console.log("typeof totalCount:",typeof totalCount);
