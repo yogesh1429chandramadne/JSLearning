a1aca568bec1ee882ab40d23eab6321db48af93c
yogesh1429chandramadne <yogesh1429.yc@gmail.com>
2023-02-18 11:51:55 +0530

    02_swapping variable
    02_swapping variable

---
+++ 02_swappVariables.js
@@ -0,0 +1,18 @@
+var sweetie = "sweetie";
+var cutie ="cutie"
+var temp = "cutie";
+cutie = sweetie;
+cutie = temp;
+console.log(sweetie , cutie);
+console.log("********* swapping using numbers********");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+var temp= num1;
+num1 = num3;
+num2 = num2;
+
+num3= temp;
+console.log(num1,num2,num3);
+
+
